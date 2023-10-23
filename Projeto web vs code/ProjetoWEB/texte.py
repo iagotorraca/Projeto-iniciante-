0771d3f2ba87a629be24b7307d225e897046f731
iago nogueira torraca <143561399+iagotorraca@users.noreply.github.com>
2023-10-23 16:46:11 -0300

    Add files via upload

---
+++ Projeto web vs code/ProjetoWEB/texte.py
@@ -0,0 +1,16 @@
+var= x,y,z,k
+x= 2
+y=4
+z=9
+k=3
+resultado=0
+if (x<y+k) and (k*2>4) and (z+k<12):
+    resultado=1
+else:
+    if (y+10<z) or (z>12):
+        resultado=2
+    else:
+        if (x+2) >= (y-4):
+            resultado=3
+        else:
+            resultado=4
